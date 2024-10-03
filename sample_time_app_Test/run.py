16e1745c1dc09d914ca376eba1344e56ad64add3
Gitesh Chinawalkar <47468867+GiteshChinawalkar@users.noreply.github.com>
2024-10-03 14:38:59 -0400

    TimeApp

---
+++ sample_time_app_Test/run.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+@app.route('/time')
+def get_time():
+    # Get the current day of the week
+    return str(datetime.datetime.now().strftime('%Y %H:%M:%S'))
+    
+app.run(host='0.0.0.0', port=8080, debug=True)
